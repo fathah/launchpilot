7e542ced9828a3e1c7d67a69ca2d7a99a5d36cce
Fathah KA <48355244+fathah@users.noreply.github.com>
2026-05-07 10:34:01 +0400

    Initial Commit

---
+++ launchpilot/launchpilotApp.swift
@@ -0,0 +1,17 @@
+//
+//  launchpilotApp.swift
+//  launchpilot
+//
+//  Created by Fathah on 07/05/26.
+//
+
+import SwiftUI
+
+@main
+struct launchpilotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ launchpilotTests/launchpilotTests.swift
@@ -0,0 +1,17 @@
+//
+//  launchpilotTests.swift
+//  launchpilotTests
+//
+//  Created by Fathah on 07/05/26.
+//
+
+import Testing
+@testable import launchpilot
+
+struct launchpilotTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
